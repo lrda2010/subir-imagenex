2c53b7ea4bee235e1b88375c4a66b1f92b328c5b
Administrador <lrda@outlook.com>
2016-01-28 11:25:57 -0500

    Subir Imagenes

--- app/controllers/CloudAPI.java
+++ app/controllers/CloudAPI.java
@@ -9,9 +9,9 @@ import java.io.IOException;
 public class CloudAPI {
 
     Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "hpvzktqsn",
-            "api_key", "549348623392329",
-            "api_secret", "PsuDIRaDkjZsNTqTl5Jw6bfm7ss"));
+            "cloud_name", "dgki96asv",
+            "api_key", "418528464326265",
+            "api_secret", "xGS_MGnlkQ5GwYvmUG6onAltPwM"));
 
      void subida(File path) throws IOException {
          cloudinary.uploader().upload(path,ObjectUtils.emptyMap());
